1652d081e3696ff92ac8b1335f8320dd2d6e1b4e
AveryCS <averydcs@gmail.com>
2023-05-02 14:46:27 -0400

    Adding composition vs. inheritance

---
+++ src/main/java/com/oopconcepts/interviewconcepts/InheritanceVsComposition.java
@@ -0,0 +1,17 @@
+package com.oopconcepts.interviewconcepts;
+
+/*
+* Inheritance vs. Composition
+Composition and inheritance are two mechanisms  used in OOP to create new classes, using other classes. Composition  can be seen through the “Has-A” relationship, while Inheritance can be seen through the “Is-A” relationship the keywords for inheritance are implements and extends.
+*
+* Key Interview question
+Why is composition better than inheritance?
+There is more flexibility with composition because you can change a variable or a method in a class, without making any changes to the superclass.
+*
+* Link to an even more detailed explanation: https://www.w3resource.com/java-tutorial/inheritance-composition-relationship.php
+
+*
+*
+* */
+public class InheritanceVsComposition {
+}
