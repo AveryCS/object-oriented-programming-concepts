d7ca586a095b83b5deb4d2696b84f3abf855eeec
AveryCS <averydcs@gmail.com>
2023-05-02 15:17:40 -0400

    Adding http requests overview

---
+++ src/main/java/com/oopconcepts/interviewconcepts/HTTPRequestsOverview.java
@@ -0,0 +1,28 @@
+package com.oopconcepts.interviewconcepts;
+/*
+* HTTP requests
+
+HTTP stands for hypertext transfer protocols, which is a protocol used between web servers or browsers. When a client sends a request to a server, the request returns a response.
+
+There are 3 parts of an HTTP request:
+*
+1) Request line, which contains:
+    a) Request method (ie GET, POST)
+    b) The URL of the resource being requested
+    c) The HTTP version being used
+2) Request Header: provides more information about the request
+3) Request body (optional)-contains data that is being sent with the request such as JSON
+
+There are 3 parts of an HTTP response:
+1) Status line that contains the status code
+2) Response headers: provides additional information about th response
+3) Response body: contains actual data being returned, ie JSON, HTML, an image
+*
+Key interview questions:
+What is an HTTP request?
+
+*
+*
+* */
+public class HTTPRequestsOverview {
+}
