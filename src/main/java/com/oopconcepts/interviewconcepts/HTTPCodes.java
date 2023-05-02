bcba0c91af14ead87537151b0fc0630ddfd79f28
AveryCS <averydcs@gmail.com>
2023-05-02 15:24:00 -0400

    Adding HTTP codes

---
+++ src/main/java/com/oopconcepts/interviewconcepts/HTTPCodes.java
@@ -0,0 +1,18 @@
+package com.oopconcepts.interviewconcepts;
+/*
+* Key interview question:
+What are the primary HTTP codes and their meanings?
+
+200-success
+
+300- redirects
+
+400- client sent bad request on their end
+
+500- something went wrong on our end/server side
+
+*
+*
+* */
+public class HTTPCodes {
+}
