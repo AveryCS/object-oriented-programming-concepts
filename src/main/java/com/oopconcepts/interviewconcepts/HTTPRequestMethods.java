6044e3dc77c314133456bb963cf1a0669673129a
AveryCS <averydcs@gmail.com>
2023-05-02 16:45:35 -0400

    Adding HTTP request methods

---
+++ src/main/java/com/oopconcepts/interviewconcepts/HTTPRequestMethods.java
@@ -0,0 +1,26 @@
+package com.oopconcepts.interviewconcepts;
+/*
+* HTTP Request methods, also known as HTTP verbs
+
+1) GET: retrieves a resource
+
+2) PUT: replaces a resource.
+
+3) POST: used to create a resource
+4) PATCH: only updates the field specified in the request
+5) DELETE: deletes the resource
+6) HEAD: retrieves the headers of a resource, but not the body
+
+**Idempotent: means it can be performed multiple times with the same result
+
+Key interview questions:
+What are the common HTTP request methods?
+
+Which of the HTTP methods are idempotent?
+-GET, PUT, and DELETE are all idempotent
+
+*
+*
+* */
+public class HTTPRequestMethods {
+}
