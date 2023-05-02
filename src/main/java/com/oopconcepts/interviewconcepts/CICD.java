b9a122438c66f19dd12756294c54d0f1f7e27429
AveryCS <averydcs@gmail.com>
2023-05-02 17:35:37 -0400

    Adding CI CD info

---
+++ src/main/java/com/oopconcepts/interviewconcepts/CICD.java
@@ -0,0 +1,24 @@
+package com.oopconcepts.interviewconcepts;
+/*
+* CI/CD stands for continuous integration/continuous deployment.
+
+Continuous integration is the practice of frequently merging all code changes. The benefit is that bugs can be fixed earlier on because they are part of the codebase early and when code is merged, it triggers automatic testing.
+
+Continuous deployment allows for applications to be automatically deployed, when a certain set of criteria for code releases are met.
+
+Some benefits of implementing CI/CD are:
+-Faster time to market: allows software changes to be released quickly
+-Improved software quality: by automating the build, test, and deployment processes, CI/CD can catch errors and bugs early and reduce likelihood of them going into production
+-Continuous feedback: developers can can quickly identify and address issues
+
+Visit this link for more info: https://about.gitlab.com/topics/ci-cd/
+
+Key interview questions:
+What is CI/CD, and how does it work?
+What are the benefits of implementing CI/CD in software development?
+Can you give an example of how you have used CI/CD to improve the development process in a previous project?
+
+*
+* */
+public class CICD {
+}
