3b4d52494a8d7cfc0a49b38b444b131c6b60c778
AveryCS <averydcs@gmail.com>
2023-05-02 17:49:29 -0400

    Adding RESTful routing info

---
+++ src/main/java/com/oopconcepts/interviewconcepts/RESTfulRouting.java
@@ -0,0 +1,24 @@
+package com.oopconcepts.interviewconcepts;
+/*RESTful routing is a pattern of creating API that map REST architecture to HTTP request methods. Each URL is known as an endpoint and specifies the resource name and its unique identifier.
+
+For example, lets say we have an API for managing a collection of books. The endpoint might look like:
+
+**The example below was copy/pasted from ChatGPT**
+
+/books/123
+
+where 123 is the unique identifier of the book in question.
+Using RESTful routing, we can map HTTP request methods to specific CRUD operations on the /books resource. For example:
+GET /books: Retrieve a list of all books
+POST /books: Create a new book
+GET /books/123: Retrieve the book with ID 123
+PUT /books/123: Update the book with ID 123
+DELETE /books/123: Delete the book with ID 123
+
+Key interview question:
+What is RESTful routing?
+
+*
+* */
+public class RESTfulRouting {
+}
